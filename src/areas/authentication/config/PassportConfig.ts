fe5bb9073b50b89f9b4224340370647fda4e1d6f
kailinwei <kailinwei95@gmail.com>
2021-04-08 21:46:07 -0700

    first authentation

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -3,4 +3,47 @@
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-export default class PassportConfig {}
+
+import { MockAuthenticationService } from "../services/Authentication.service.mock";
+import * as passportLocal from 'passport-local';
+import { Strategy } from "passport";
+const LocalStrategy = passportLocal.Strategy;
+
+export default class PassportConfig {
+    private _user;
+    private _strategy;
+    private _service;
+
+    constructor(service:MockAuthenticationService){
+        this._service = service;
+        this._strategy = new LocalStrategy(
+
+        {
+            usernameField: "email",
+            passwordField: "password",
+        },
+        async (email,password,done) =>{
+            this._user = await this._service.getUserByEmailAndPassword(email,password)
+
+            return this._user
+            ? done(null, this._user)
+            : done (null, false, {
+                message : "Your login details are not valid. Please try again."
+            })
+        }
+        )
+    }
+
+    public get user () {
+        return this._user;
+    }
+
+    public get service(): MockAuthenticationService {
+        return this._service;
+    }
+
+    public get strategy(): passportLocal.Strategy {
+        return this._strategy;
+    }
+
+}
--- src/middleware/passport.middlewares.ts
+++ src/middleware/passport.middlewares.ts
@@ -1,8 +1,28 @@
+import { MockAuthenticationService } from "../areas/authentication/services";
 import passport from "passport";
 import PassportConfig from "../areas/authentication/config/PassportConfig";
+import IUser from "../interfaces/user.interface";
 
 module.exports = (app) => {
   app.use(passport.initialize());
   app.use(passport.session());
   // Use PassportConfig class here
+
+const localLogin = new PassportConfig(new MockAuthenticationService);
+
+passport.serializeUser((user:IUser, done) => {
+  done(null, user.email);
+});
+
+passport.deserializeUser(async (email:string, done) => {
+  let user = await localLogin.service.findUserByEmail(email);
+  if (user) {
+      done(null, user);
+  } else {
+      done(new Error('user ID is invalid'));
+  }
+});
+
+passport.use(localLogin.strategy);
+
 };
