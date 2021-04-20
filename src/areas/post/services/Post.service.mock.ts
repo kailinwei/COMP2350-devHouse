907fcc3ca31c6b1db68274ddc9c9c55d8c995e6c
Hailey Kim <haileykim@MacBook-Pro-3.local>
2021-04-19 19:31:52 -0700

    post updated

--- src/areas/post/services/Post.service.mock.ts
+++ src/areas/post/services/Post.service.mock.ts
@@ -1,14 +1,35 @@
 import IPost from "../../../interfaces/post.interface";
 import IPostService from "./IPostService";
+import {database, posts, post} from "../../../model/fakeDB"
 
 // ⭐️ Feel free to change this class in any way you like. It is simply an example...
 export class MockPostService implements IPostService {
   addPost(post: IPost, username: string): void {
-    // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+   
+    let newPost = {
+      id: post.id,
+      userId: username,
+      createdAt: new Date(),
+      message: post.message,
+      comments: 0,
+      reposts: 0,
+      likes: 0,
+      commentList: [],
+    };
+    
+    const users = database.users;
+    const userFind = users.find((element) => element.id == post.id);
+    userFind.posts.push(newPost);
+      throw new Error("Method not implemented.");
   }
+
   getAllPosts(username: string): IPost[] {
-    // 🚀 Implement this yourself.
+    
+    const users = database.users;
+    // const post= users.find(
+    //   async (element) => element.username == username){
+    //     await 
+    //   };
     throw new Error("Method not implemented.");
   }
   findById(id: string): IPost {
